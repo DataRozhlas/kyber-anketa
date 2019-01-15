eb26a773bedae3908b2c16a80411066fd5a2d21c
timichal <michal@zlatkovsky.cz>
2019-01-15 22:51:55 +0100

    jquery fix

--- js/script.js
+++ js/script.js
@@ -3,14 +3,15 @@ import flexibility from "flexibility";
 
 function Rendruj(data, dodivu) {
   $(data).each(function(i) {
-      $("<div class='respondent'></div>").appendTo(dodivu);
-      $(".respondent").last().append("<img class='portret' src='https://data.irozhlas.cz/kyber-anketa/img/" + this.f + "'>");
-      $(".respondent").last().append("<p class='jmeno'><strong>" + this.j + " " + this.p + "</strong>, " + this.s + "</p>");
-      if (!this.o1) {$(".respondent").last().append("<p class='veta'><span class='bezodpovedi'>bez odpovědi</span>")};
-      if (this.o1) {$(".respondent").last().append("<p class='veta'><span class='cislo' title='Použil/a jste někdy pracovní e-mail k soukromým účelům?'>1</span>" + this.o1 + "</p>")};
-      if (this.o2) {$(".respondent").last().append("<p class='veta'><span class='cislo' title='Změnil/a jste v souvislosti s aférou v Německu své chování na internetu?'>2</span>" + this.o2 + "</p>")};
-      if (this.o3) {$(".respondent").last().append("<p class='veta'><span class='cislo' title='Kolik hesel na internetu používáte?'>3</span>" + this.o3 + "</p>")};
-      if (this.o4) {$(".respondent").last().append("<p class='veta'><span class='cislo' title='Používáte správce hesel?'>4</span>" + this.o4 + "</p>")};
+      var respondentDiv = $("<div class='respondent'></div>");
+      $(respondentDiv).append("<img class='portret' src='https://data.irozhlas.cz/kyber-anketa/img/" + this.f + "'>");
+      $(respondentDiv).append("<p class='jmeno'><strong>" + this.j + " " + this.p + "</strong>, " + this.s + "</p>");
+      if (!this.o1) {$(respondentDiv).append("<p class='veta'><span class='bezodpovedi'>bez odpovědi</span>")};
+      if (this.o1) {$(respondentDiv).append("<p class='veta'><span class='cislo' title='Použil/a jste někdy pracovní e-mail k soukromým účelům?'>1</span>" + this.o1 + "</p>")};
+      if (this.o2) {$(respondentDiv).append("<p class='veta'><span class='cislo' title='Změnil/a jste v souvislosti s aférou v Německu své chování na internetu?'>2</span>" + this.o2 + "</p>")};
+      if (this.o3) {$(respondentDiv).append("<p class='veta'><span class='cislo' title='Kolik hesel na internetu používáte?'>3</span>" + this.o3 + "</p>")};
+      if (this.o4) {$(respondentDiv).append("<p class='veta'><span class='cislo' title='Používáte správce hesel?'>4</span>" + this.o4 + "</p>")};
+      respondentDiv.appendTo(dodivu)
      });
 }
 
